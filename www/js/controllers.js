6f05d432df67bf88b2ec2d076296573a015624c0
Justin Noel <github@calendee.com>
2014-02-12 01:54:19 -0600

    First commit of www src.
    Signed-off-by: Justin Noel <github@calendee.com>

---
+++ www/js/controllers.js
@@ -0,0 +1,23 @@
+angular.module('starter.controllers', [])
+
+
+// A simple controller that fetches a list of data from a service
+.controller('PetIndexCtrl', function($scope, PetService) {
+  // "Pets" is a service returning mock data (services.js)
+  $scope.pets = PetService.all();
+})
+
+
+// A simple controller that shows a tapped item's data
+.controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
+  // "Pets" is a service returning mock data (services.js)
+  $scope.pet = PetService.get($stateParams.petId);
+})
+
+.controller('SimpleFormCtrl', function($scope, $location) {
+
+	$scope.submit = function() {
+		
+		$location.url('/submitted');
+	}
+})
